67acc839fc35710eed5434d470e94ff5a73ec0e8
Gxnza <gonzalobonadeo07@gmial.com>
2025-05-08 20:27:43 -0300

    Primer commit

---
+++ src/types/index.ts
@@ -0,0 +1,22 @@
+export interface Product {
+  id: string;
+  name: string;
+  price: number;
+  description: string;
+  image: string;
+  additionalImages: string[];
+  category: string;
+  details: string[];
+  sizes: string[];
+}
+
+export interface CartItem {
+  product: Product;
+  quantity: number;
+  size: string;
+}
+
+export interface CustomerInfo {
+  firstName: string;
+  lastName: string;
+}
